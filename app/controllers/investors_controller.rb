dcdac738e726a0a24f4b4a2530ccfee0074fa417
Alex <alexandralaudon@gmail.com>
2015-06-29 16:49:10 +0200

    Cleaned up CRUD for investors controller

--- app/controllers/investors_controller.rb
+++ app/controllers/investors_controller.rb
@@ -1,11 +1,50 @@
 class InvestorsController < ApplicationController
 
   def index
+    @investors = Investor.order(rank: :desc)
+  end
 
+  def new
+    @investor = Investor.new
   end
 
   def create
-
+    @investor = Investor.new(investor_params)
+    if @investor.save
+      flash[:notice] = "#{@investor.first_name} #{@investor.last_name} has been saved to the database."
+      redirect_to investors_path
+    else
+      flash[:notice] = "Uh-oh, something went wrong and this investor was not saved. Try again."
+      render :new
+    end
   end
-  
+
+  def edit
+    @investor = Investor.find(params[:id])
+  end
+
+  def update
+    @investor = Investor.find(params[:id])
+    if @investor.update(investor_params)
+      flash[:notice] = "#{@investor.first_name} #{@investor.last_name} has been updated!"
+      redirect_to investors_path
+    else
+      flash[:notice] = "Uh-oh, something went wrong and this investor was not updated. Try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @investor = Investor.find(params[:id])
+    @investor.destroy
+    flash[:notice] = "#{@investor.first_name} #{@investor.last_name} has been deleted from the system."
+    redirect_to investors_path
+  end
+
+  private
+
+  def investor_params
+    params.require(:investor).permit(:first_name, :last_name, :company, :email, :rank)
+  end
+
 end
