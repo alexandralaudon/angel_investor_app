d7a07e9efd7f3a76a9c69f989bc6811c47c4da5f
Alex <alexandralaudon@gmail.com>
2015-06-30 16:00:34 +0200

    converted all html.erb files to HAML

--- app/controllers/endorsements_controller.rb
+++ app/controllers/endorsements_controller.rb
@@ -1,14 +1,14 @@
 class EndorsementsController < ApplicationController
 
   def new
-    @endorsements = Endorsement.new
+    @endorsement = Endorsement.new
   end
 
   def create
-    @endorsements = Endorsement.new(endorsement_params)
-    if @endorsements.save
-      Investor.find(@endorsements.endorsed).increment!(:rank)
-      flash[:notice] = "#{@endorsements.investor.full_name}, thank you for adding your endorsement!"
+    @endorsement = Endorsement.new(endorsement_params)
+    if @endorsement.save
+      Investor.find(@endorsement.endorsed).increment!(:rank)
+      flash[:notice] = "#{@endorsement.investor.full_name}, thank you for adding your endorsement!"
       redirect_to root_path
     else
       flash[:notice] = "Uh-oh, something went wrong and this endorsement was not saved. Please try again."
