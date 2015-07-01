c5ae5155ee2719d2716186cce68c592935de6c5d
Alex <alexandralaudon@gmail.com>
2015-07-01 10:37:11 +0200

    Added basic form validations to investors & endorsements

--- app/models/endorsement.rb
+++ app/models/endorsement.rb
@@ -2,6 +2,10 @@ class Endorsement < ActiveRecord::Base
   belongs_to :investor
   belongs_to :endorsed, :class_name => 'Investor'
 
+  validates :investor_id, presence: true
+  validates :endorsed_id, presence: true
+  validates :comment, presence: true
+
   validate :unique_endorsement
 
   def unique_endorsement
--- app/models/investor.rb
+++ app/models/investor.rb
@@ -2,6 +2,10 @@ class Investor < ActiveRecord::Base
   has_many :endorsements
   has_many :endorsed, through: :endorsements
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+
   def full_name
     "#{first_name} #{last_name}".titleize
   end
